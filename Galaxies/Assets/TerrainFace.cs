366e75474c658ac29be571c52c03937acce01965
Max Andreassen <andreassenmax@gmail.com>
2020-06-22 17:35:49 +0100

    Initial Planet Generator Added

--- Galaxies/Assets/TerrainFace.cs
+++ Galaxies/Assets/TerrainFace.cs
@@ -1,16 +1,59 @@
 ﻿using UnityEngine;
 
-public class TerrainFace : MonoBehaviour
+public class TerrainFace
 {
-    // Start is called before the first frame update
-    void Start()
+    Mesh mesh;
+    int resolution;
+    Vector3 localUp;
+
+    Vector3 axisA;
+    Vector3 axisB;
+
+    public TerrainFace(Mesh mesh, int resolution, Vector3 localUp)
     {
-        
+        this.mesh = mesh;
+        this.resolution = resolution;
+        this.localUp = localUp;
+
+        axisA = new Vector3(localUp.y, localUp.z, localUp.x);
+        axisB = Vector3.Cross(localUp, axisA);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void ConstructMesh()
     {
-        
+        var vertices = new Vector3[resolution * resolution];
+        var triangles = new int[(resolution - 1) * (resolution - 1) * 6];
+        var triIndex = 0;
+
+        for (var y = 0; y < resolution; y++)
+        {
+            for (var x = 0; x < resolution; x++)
+            {
+                var i = x + y * resolution;
+                var percent = new Vector2(x, y) / (resolution - 1);
+                var pointOnUnitCube = localUp + (percent.x - 0.5f) * 2 * axisA + (percent.y - 0.5f) * 2 * axisB;
+                var pointOnUnitSphere = pointOnUnitCube.normalized;
+
+                vertices[i] = pointOnUnitSphere;
+
+                if (x != resolution - 1 && y != resolution - 1)
+                {
+                    triangles[triIndex] = i;
+                    triangles[triIndex + 1] = i + resolution + 1;
+                    triangles[triIndex + 2] = i + resolution;
+
+                    triangles[triIndex + 3] = i;
+                    triangles[triIndex + 4] = i + 1;
+                    triangles[triIndex + 5] = i + resolution + 1;
+
+                    triIndex += 6;
+                }
+            }
+        }
+
+        mesh.Clear();
+        mesh.vertices = vertices;
+        mesh.triangles = triangles;
+        mesh.RecalculateNormals();
     }
 }
