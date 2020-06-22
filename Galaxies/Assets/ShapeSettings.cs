75e1109b9d1cd1bf51fd1ef89e42704753bd4911
Max Andreassen <andreassenmax@gmail.com>
2020-06-22 19:29:04 +0100

    Planet Noise Added

--- Galaxies/Assets/ShapeSettings.cs
+++ Galaxies/Assets/ShapeSettings.cs
@@ -4,4 +4,15 @@
 public class ShapeSettings : ScriptableObject
 {
     public float planetRadius = 1;
+    public NoiseLayer[] noiseLayers;
+
+    [System.Serializable]
+    public class NoiseLayer
+    {
+        public bool enabled = true;
+
+        public bool useFirstLayerAsMask;
+
+        public NoiseSettings noiseSettings;
+    }
 }
