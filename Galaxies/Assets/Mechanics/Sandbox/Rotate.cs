5c3672675f45f0eecb53c789da3c68134938ff7f
Max Andreassen <andreassenmax@gmail.com>
2020-06-26 21:44:46 +0100

    Planet Rotation

---
+++ Galaxies/Assets/Mechanics/Sandbox/Rotate.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+    public float speed = 1f;
+
+    float modifier = 1f;
+
+    private void Start()
+    {
+        modifier = Random.Range(0.5f, 1f);
+
+        var flip = Random.Range(0, 2);
+
+        if (flip == 1)
+            modifier *= -1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(new Vector3(Time.deltaTime * speed * modifier, Time.deltaTime * speed, 0));
+    }
+}
